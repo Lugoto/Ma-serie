9997072a64a68b2bd69b3b39a1f0d1d69c288467
nruiz64 <ruiz.nicolas64@gmail.com>
2016-04-13 13:48:07 +0200

    REfacto

--- index.php
+++ index.php
@@ -59,10 +59,12 @@
 
         </div>
         </div>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
-        <script type="text/javascript" src="js/materialize.js"></script>
-        <script type="text/javascript" src="js/script.js"></script>
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+
         <script>
             $(document).ready(function () {
                 $(".button-collapse").sideNav();
